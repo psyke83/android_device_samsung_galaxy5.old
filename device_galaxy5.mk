131c184236bf796da0cf40ca986a4a4570eaaf74
Conn O'Griofa <connogriofa@gmail.com>
2011-10-06 16:45:17 +0100

    Keep backup of single and multitouch permission files (for madconfig/fake dual-touch driver)

--- device_galaxy5.mk
+++ device_galaxy5.mk
@@ -49,7 +49,12 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml
+
+# Hardware properties: keep dual touchscreen permissions available for madconfig script
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/touchscreen/android.hardware.touchscreen.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/touchscreen/android.hardware.touchscreen.multitouch.distinct.xml
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES += \
