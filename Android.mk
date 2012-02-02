fef9d96670d52ba17b6624339a79e93de75a0d86
Conn O'Griofa <connogriofa@gmail.com>
2012-02-02 11:01:06 +0000

    Android.mk: build all makefiles, remove setup_fs stuff

--- Android.mk
+++ Android.mk
@@ -16,14 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),galaxy5)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := setup_fs.c
-LOCAL_MODULE := setup_fs
-LOCAL_MODULE_TAGS := optional
-#LOCAL_SHARED_LIBRARIES += libext4_utils libz
-include $(BUILD_EXECUTABLE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),galaxy5)
+    include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
