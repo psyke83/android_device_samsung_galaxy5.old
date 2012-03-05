cdf58dd2e14355c7ef2e727d7007a8ce3ae3226d
Conn O'Griofa <connogriofa@gmail.com>
2012-03-05 07:44:43 +0000

    vendorsetup.sh: rename vendor setup name

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_galaxy5-eng
+add_lunch_combo cyanogen_galaxy5-eng
