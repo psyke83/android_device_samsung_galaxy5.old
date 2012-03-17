0f62f071f5b1113a36679c8101f2934f17e9d0a4
Conn O'Griofa <connogriofa@gmail.com>
2012-03-17 23:59:18 +0000

    Remove modifications that conflict with vanilla CM7 source or are unsuitable for upstream inclusion

--- device_galaxy5.mk
+++ device_galaxy5.mk
@@ -53,8 +53,6 @@ PRODUCT_COPY_FILES += \
 # MADTeam modifications
 PRODUCT_COPY_FILES += \
     device/samsung/galaxy5/prebuilt/bin/madconfig:system/bin/madconfig \
-    device/samsung/galaxy5/prebuilt/etc/init.d/02madteam:system/etc/init.d/02madteam \
-    device/samsung/galaxy5/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
     device/samsung/galaxy5/prebuilt/modules/synaptics_i2c_rmi4.ko:system/lib/modules/synaptics_i2c_rmi4.ko \
     frameworks/base/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/touchscreen/android.hardware.touchscreen.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/touchscreen/android.hardware.touchscreen.multitouch.distinct.xml
